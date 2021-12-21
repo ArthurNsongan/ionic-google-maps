9f8c140aa28bf130828b79efceae7c457382a1f1
Arthur NSONGAN <13arthurdylannsongan@gmail.com>
2021-12-21 14:50:49 +0100

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,22 @@
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+declare const google;
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit, AfterViewInit {
+  @ViewChild('mapElement') mapNativeElement: ElementRef;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  ngAfterViewInit(): void {
+    const map = new google.maps.Map(this.mapNativeElement.nativeElement, {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    });
+  }
+
+}
